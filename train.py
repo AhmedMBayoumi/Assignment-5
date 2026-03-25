5ef6f3ca332ffd76109af2c6febea1e46dd90583
AhmedBayoumi <82550239+AMBlaxe@users.noreply.github.com>
2026-03-26 00:58:59 +0200

    10 epochs

--- train.py
+++ train.py
@@ -51,7 +51,6 @@ def train():
             running_loss = 0.0
             
             for i, data in enumerate(trainloader, 0):
-                if i >= 100: break 
                 inputs, labels = data[0].to(device), data[1].to(device)
                 optimizer.zero_grad()
                 outputs = model(inputs)
